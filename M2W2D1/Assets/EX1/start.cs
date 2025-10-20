d522e06294383e28cb09ff21c0487c9538e2784c
ThinkDifferent003 <rolandbibal3@gmail.com>
2025-10-20 19:06:33 +0200

    ex

---
+++ M2W2D1/Assets/EX1/start.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class start : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, World!");//Appare "Hello, World!" nella console di Unity all'avvio del gioco
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
