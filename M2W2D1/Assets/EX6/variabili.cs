5d0e61fa7858d54c3cc0b70c83446e2c5fadcfa4
ThinkDifferent003 <rolandbibal3@gmail.com>
2025-10-20 21:09:07 +0200

    ex6

---
+++ M2W2D1/Assets/EX6/variabili.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variabili : MonoBehaviour
+{
+    public int number = 5;
+    public int number2 = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (number < number2)
+        {
+            Debug.Log("10");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
